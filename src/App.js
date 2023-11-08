1df4070ad3b74e0b034e632524523f466d014a48
PyGularCoder <umar.a@fitinpart.sg>
2023-11-08 11:33:11 +0500

    React ka pehla project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Test from "./Test";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>PyGularCoder using create react app</h1>
+      <Test/>
+    </>
   );
 }
 
---
+++ src/Test.js
@@ -0,0 +1,10 @@
+function Test(){
+    let  a = 10;
+    let b = 30;
+    let sum = a + b;
+    return(
+        <p id="sum">{sum}</p>
+    )
+}
+
+export default Test
